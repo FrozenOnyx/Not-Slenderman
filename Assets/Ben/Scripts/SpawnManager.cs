5fcc85fcc19491b9c140d650278b055906795758
FrozenOnyx <1028835@student.sae.edu.au>
2024-03-24 21:22:25 +1000

    made spawn manager

--- Assets/Ben/Scripts/SpawnManager.cs
+++ Assets/Ben/Scripts/SpawnManager.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject Spawn1;
+    public GameObject Spawn2;
+    public GameObject Spawn3;
+    List<GameObject> spawnList = new List<GameObject>();
+    public GameObject Skull;
+    int bone = 0;
+
     // Start is called before the first frame update
     void Start()
     {
+        spawnList.Add(Spawn1);
+        spawnList.Add(Spawn2);
+        spawnList.Add(Spawn3);
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        bone = Random.Range(0, spawnList.Count);
+        Instantiate(Skull, spawnList[bone].transform.position, Quaternion.identity);
     }
 }
