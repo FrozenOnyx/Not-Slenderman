b6b0d8aa442925400f8a5402c03d56a33ce55d48
Nathan <NathanNguyen8844@gmail.com>
2024-03-24 22:43:13 +1000

    game pauses

--- Assets/Nathan/Scripts/Inventory/InventoryUI.cs
+++ Assets/Nathan/Scripts/Inventory/InventoryUI.cs
@@ -12,12 +12,14 @@ public class InventoryUI : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E) && menuActive == true)
         {
+            Time.timeScale = 1;
             InventoryMenu.SetActive(false);
             menuActive = false;
         }
 
         if (Input.GetKeyDown(KeyCode.E) && menuActive == false)
         {
+            Time.timeScale = 0;
             InventoryMenu.SetActive(true);
             menuActive = true;
         }
