8aa70c34f6747c5d1605e35f22670288bb56cbfa
FrozenOnyx <1028835@student.sae.edu.au>
2024-03-25 13:18:08 +1000

    first playable

--- Assets/Ben/Scripts/Items/SkullArea.cs
+++ Assets/Ben/Scripts/Items/SkullArea.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SkullArea : MonoBehaviour
 {
     public int skullCounter = 0;
+
+    bool isPressed = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,19 +16,24 @@ public class SkullArea : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.F))
+        {
+            Debug.Log("head");
+            isPressed = true;
+        }
+        else
+        {
+            isPressed = false;
+        }
     }
+
 
     public void OnTriggerStay(Collider other)
     {
-        if (other.gameObject == GameObject.FindWithTag("Skull"))
+        if (other.gameObject == GameObject.FindWithTag("Skull") && isPressed)
         {
-            if (Input.GetKeyDown(KeyCode.F))
-            {
-                Destroy(other.gameObject);
-                skullCounter++;
-                Debug.Log("head");
-            }
+            skullCounter++;
+            Destroy(other.gameObject);
         }
     }
 }
