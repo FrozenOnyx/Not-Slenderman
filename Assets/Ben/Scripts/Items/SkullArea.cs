8a11a7d27cdf34f363f461e3988e91b2a3c05843
FORDORDERKLUG\mockr <phoenixmockruby05@gmail.com>
2024-03-28 22:35:56 +1000

    fixes

--- Assets/Ben/Scripts/Items/SkullArea.cs
+++ Assets/Ben/Scripts/Items/SkullArea.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SkullArea : MonoBehaviour
 {
     public int skullCounter = 0;
-
-    public bool inside = false;
+    public GameObject skull;
     public bool destroy = false;
     // Start is called before the first frame update
     void Start()
@@ -17,37 +17,25 @@ public class SkullArea : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.F) && inside)
+        if (Input.GetKey(KeyCode.F))
         {
             Debug.Log("head");
             destroy = true;
+        }
+        else
+        {
+            destroy = false;
         }
     }
 
 
     public void OnTriggerStay(Collider other)
     {
-        if (other.gameObject == GameObject.FindWithTag("Skull") && destroy)
+        if (other.gameObject.CompareTag("Skull") && destroy)
         {
             skullCounter++;
             Destroy(other.gameObject);
-            destroy = false;
-            inside = false;
   
-        }
-    }
-    public void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject == GameObject.FindWithTag("Skull"))
-        {
-            inside = true;
-        }
-    }
-    public void OnTriggerExit(Collider other)
-    {
-        if(other.gameObject == GameObject.FindWithTag("Skull"))
-        {
-            inside = false;
         }
     }
 }
