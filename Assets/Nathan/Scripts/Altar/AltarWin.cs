eec7f606162b296c5964757baa3de077cb7c89b5
SAE1\1030021 <1030021@student.sae.edu.au>
2024-04-08 11:18:20 +1000

    improvement of inventory logic and improvement of win condition logic

--- Assets/Nathan/Scripts/Altar/AltarWin.cs
+++ Assets/Nathan/Scripts/Altar/AltarWin.cs
@@ -9,13 +9,17 @@ public class AltarWin : MonoBehaviour
     public SkullArea skullArea;
     public GameObject winScreen;
 
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.name == "Altar")
         {
-            if (skullArea.skullCounter == 8)
+
+            Debug.Log("Colliding with altar");
+
+            if (skullArea.skullCounter == 8 && Input.GetKey(KeyCode.F))
             {
                 winScreen.SetActive(true);
+                Time.timeScale = 0;
             }
         }
     }
