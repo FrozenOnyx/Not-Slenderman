48268cb9b2891a9dcecde4a54b0960ad2ca20cc7
SAE1\1030021 <1030021@student.sae.edu.au>
2024-04-29 11:16:10 +1000

    finished pausing menu

--- Assets/Nathan/Scripts/Menus/PauseMenu.cs
+++ Assets/Nathan/Scripts/Menus/PauseMenu.cs
@@ -1,18 +1,51 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject pauseMenu;
+    private bool menuActive = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape) && menuActive == true)
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+            menuActive = false;
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && menuActive == false)
+        {
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+            menuActive = true;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+        menuActive = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void QuitToMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void QuitToDesktop()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
     }
 }
