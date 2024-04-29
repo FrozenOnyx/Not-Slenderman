32f6ad3b24667e9067e421e060eaa2e8fa2c460e
SAE1\1030021 <1030021@student.sae.edu.au>
2024-04-29 12:00:45 +1000

    credits added into pause menu

--- Assets/Nathan/Scripts/Menus/PauseMenu.cs
+++ Assets/Nathan/Scripts/Menus/PauseMenu.cs
@@ -7,6 +7,7 @@ public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;
     private bool menuActive = false;
+    public GameObject creditsMenu;
 
     // Update is called once per frame
     void Update()
@@ -47,5 +48,17 @@ public class PauseMenu : MonoBehaviour
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    public void CreditsMenuOpen()
+    {
+        creditsMenu.SetActive(true);
+        pauseMenu.SetActive(false);
+    }
+
+    public void CreditsMenuClose()
+    {
+        creditsMenu.SetActive(false);
+        pauseMenu.SetActive(true);
     }
 }
